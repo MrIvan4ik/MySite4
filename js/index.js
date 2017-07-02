58d7b82ed347a846785f5b6296ea8dc860da345c
Ivan <ivan@localhost.localdomain>
2017-07-02 19:39:28 +0300

    Добавил отправку данных и проверку email

--- js/index.js
+++ js/index.js
@@ -1,7 +1,26 @@
 function scrollTo(hash) {
     location.hash = hash;
-}
+};
 window.onload = function () {
   document.getElementById("body").className = ""
   document.getElementById("preloader").className = "preloader-none"
+};
+
+
+function sendContact() {
+
+  let contact = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    text: document.getElementById("text").value
+  };
+
+
+  function checkEmail(email) {
+    if (email.indexOf("@") == -1 || email.indexOf(".") == -1) {
+      contact.email = "Invalid"
+    };
+  };
+  checkEmail(contact.email);
+  alert(JSON.stringify(contact))
 }
